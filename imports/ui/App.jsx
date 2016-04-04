14df63c2b148ec144529f79d2c241d7348e58596
happywhilecoding <jon@expungeus>
2016-04-03 21:42:34 -0700

    Created database connection for guesses (local only)

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,43 +1,45 @@
 import React, { Component, PropTypes } from 'react';
 import { createContainer } from 'meteor/react-meteor-data';
-
 import { Guesses } from '../api/guesses.js';
-
 import Guess from './Guess.jsx';
 
 // App component - represents the whole app
-export default class App extends Component {
-  getGuesses() {
-    return [
-      { _id: 1, text: 'This is guess 1' },
-      { _id: 2, text: 'This is guess 2' },
-      { _id: 3, text: 'This is guess 3' },
-    ];
-  }
 
+class App extends Component {
   renderGuesses() {
-    return this.getGuesses().map((guess) => (
+    return this.props.guesses.map((guess) => (
       <Guess key={guess._id} guess={guess} />
     ));
   }
-
   render() {
     return (
-      <div class="container-fluid">
-      <div class="row">
-        <div className="col-md-3"></div>
-        <div className="col-md-6">
-          <img src="/exes.gif" className="text-left" className="gif" />
-          <h1 className="centered">Welcome to Hedge!</h1>
-          <div className="centered">
-            <button className="btn btn-default btn-lg" type="button">Let's Begin</button>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3"></div>
+          <div className="col-md-6">
+            <img src="/exes.gif" className="text-left" className="gif" />
+            <h1 className="centered">Welcome to Hedge!</h1>
+            <div className="centered">
+              <button className="btn btn-default btn-lg" type="button">Let's Begin</button>
+            </div>
+            {this.renderGuesses()}
+            <p>You want to date enough people to get a sense of your options, but you don't want to leave the choice too long and risk missing your ideal match. You need some kind of formula that balances the risk of stopping too soon against the risk of stopping too late.</p>
+            <p>If you just choose randomly, your odds of picking the best of 11 suitors is about 9 percent. But if you use this method above, the probability of picking the best of the bunch increases significantly, to 37 percent — not a sure bet, but much better than random.</p>
           </div>
-          <p>You want to date enough people to get a sense of your options, but you don't want to leave the choice too long and risk missing your ideal match. You need some kind of formula that balances the risk of stopping too soon against the risk of stopping too late.</p>
-          <p>If you just choose randomly, your odds of picking the best of 11 suitors is about 9 percent. But if you use this method above, the probability of picking the best of the bunch increases significantly, to 37 percent — not a sure bet, but much better than random.</p>
+          <div className="col-md-3"></div>
         </div>
-        <div className="col-md-3"></div>
-      </div>
     </div>
     );
   }
 }
+
+
+App.propTypes = {
+  guesses: PropTypes.array.isRequired,
+};
+
+export default createContainer(() => {
+  return {
+    guesses: Guesses.find({}).fetch(),
+  };
+}, App);
--- imports/ui/Guess.jsx
+++ imports/ui/Guess.jsx
@@ -1,10 +1,16 @@
 import React, { Component, PropTypes } from 'react';
 
 // Guess component - represents a single guess item
+
 export default class Guess extends Component {
   render() {
     return (
-      <li>{this.props.guess.text}</li>
+        <div>
+          <li>AGE: {this.props.guess.age}</li>
+          <li>STARTED DATING AT : {this.props.guess.startDate}</li>
+          <li>SUITORS SO FAR: {this.props.guess.suitorsSoFar}</li>
+          <li>GUESSED LIFE SPAN: {this.props.guess.lifespan}</li>
+        </div>
     );
   }
 }
